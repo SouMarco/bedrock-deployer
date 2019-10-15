e118ec82feecf7bcfa030c6d33f9a5a8561ed65d
Marco Amaral <maamaral@dgae.mec.pt>
2019-10-15 09:52:21 +0100

    On backup files, give backup file name a description

--- recipe/filetransfer.php
+++ recipe/filetransfer.php
@@ -67,7 +67,10 @@ task('backup:remote_files', function () {
 
 	foreach (get('sync_dirs') as $localDir => $serverDir) {
 
-		$backupFilename = '_remote_upload_backup_' . date('Y-m-d_H-i-s') . '.zip';
+        //get folder name
+        preg_match('/([a-zA-z0-9])+\/$/', $serverDir, $folderName);
+
+		$backupFilename = '_remote_' . substr($folderName[0], 0, -1) . '_backup_' . date('Y-m-d_H-i-s') . '.zip';
 
     // Note: sync_dirs can have a trailing slash (which means, sync only the content of the specified directory)
 		if (substr($serverDir, -1) == '/') {
@@ -89,7 +92,10 @@ task('backup:local_files', function () use ($getUBPath) {
 
 	foreach (get('sync_dirs') as $localDir => $serverDir) {
 
-		$backupFilename = '_local_upload_backup_' . date('Y-m-d_H-i-s') . '.zip';
+        //get folder name
+        preg_match('/([a-zA-z0-9])+\/$/', $serverDir, $folderName);
+
+		$backupFilename = '_local_' . substr($folderName[0]) . '_backup_' . date('Y-m-d_H-i-s') . '.zip';
 
   	// Note: sync_dirs can have a trailing slash (which means, sync only the content of the specified directory)
 		if (substr($localDir, -1) == '/') {
